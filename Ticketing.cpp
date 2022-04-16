2d0f6dc3eb73cae6bd1e8cf74f2d684d6f744d86
kimsaeng <97522890+kimsaeng@users.noreply.github.com>
2022-04-16 11:37:08 +0900

    Update Ticketing.cpp

--- Ticketing.cpp
+++ Ticketing.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//const int naming will be updated
+
 int main(void) {
 	int ticketType = 0;
 	int ticketTime = 0;
@@ -10,35 +10,40 @@ int main(void) {
 	int birthDay;
 	int manAge;
 	int idNum[13];
-	int ticketPrice = 0;
-	int ticketNum = 1; // fixed
+	int price = 0;
+	int qty = 1; // quantity fixed
 	int ridesOrNot;
 	int specialOffer = 0;
 	int totalPrice = 0;
 	int continueOrNot;
-	const int THISYEAR = 2022;
+	const int THIS_YEAR = 2022;
 	const int TODAY = 416;
-	const int ALLADULT1DAY = 62000;
-	const int ALLYOUTH1DAY = 54000;
-	const int ALLCHILD1DAY = 47000;	
-	const int ALLADULTAFTER4 = 50000;
-	const int ALLYOUTHAFTER4 = 43000;
-	const int ALLCHILDAFTER4 = 36000;	
-	const int PARKADULT1DAY = 59000;
-	const int PARKYOUTH1DAY = 52000;
-	const int PARKCHILD1DAY = 46000;	
-	const int PARKADULTAFTER4 = 47000;
-	const int PARKYOUTHAFTER4 = 41000;
-	const int PARKCHILDAFTER4 = 35000;	
+	const int ALL_ADULT_1DAY = 62000;
+	const int ALL_YOUTH_1DAY = 54000;
+	const int ALL_CHILD_1DAY = 47000;	
+	const int ALL_ADULT_AFTER4 = 50000;
+	const int ALL_YOUTH_AFTER4 = 43000;
+	const int ALL_CHILD_AFTER4 = 36000;	
+	const int PARK_ADULT_1DAY = 59000;
+	const int PARK_YOUTH_1DAY = 52000;
+	const int PARK_CHILD_1DAY = 46000;	
+	const int PARK_ADULT_AFTER4 = 47000;
+	const int PARK_YOUTH_AFTER4 = 41000;
+	const int PARK_CHILD_AFTER4 = 35000;	
 	const int BABY = 15000;
-	const int FREEOFCHARGE = 0;	
+	const int FREE_OF_CHARGE = 0;
+	const float DISABLED_DC_RATE = 0.5;
+	const float VETERAN_DC_RATE = 0.5;
+	const float MILITARY_DC_RATE = 0.49;
+	const float PREGNANT_DC_RATE = 0.5;
+	const float MULTICHILD_DC_RATE = 0.3;
 	//2D Array
 	int ticketInfo[10][5] = {0};
-	const int ticketTypeIndex = 0;
-	const int ticketTimeIndex = 1;
-	const int manAgeIndex = 2;
-	const int ticketPriceIndex = 3;
-	const int specialOfferIndex = 4;
+	const int TICKET_TYPE_INDEX = 0;
+	const int TICKET_TIME_INDEX = 1;
+	const int MANAGE_INDEX = 2;
+	const int PRICE_INDEX = 3;
+	const int SPCIAL_OFFER_INDEX = 4;
 
 	while(true) {
 	do {	 
@@ -60,7 +65,7 @@ int main(void) {
 	}
 	
 	birthDay = idNum[2]*1000+idNum[3]*100+idNum[4]*10+idNum[5];
-	manAge = THISYEAR - ageYear;
+	manAge = THIS_YEAR - ageYear;
 	if(birthDay >= TODAY) {
 		manAge;
 	} else {
@@ -68,82 +73,82 @@ int main(void) {
 	}
 	// price setting
 	if(manAge < 1) {
-		ticketPrice = FREEOFCHARGE;	
+		price = FREE_OF_CHARGE;	
 	} else if (manAge >= 1  && manAge < 3) {
 		printf("baby rides? 1.yes 2.no\n");
 		scanf("%d", &ridesOrNot);
 		if(ridesOrNot == 1) {
-			ticketPrice = BABY;
+			price = BABY;
 		} else if(ridesOrNot == 2) {
-			ticketPrice = FREEOFCHARGE;
+			price = FREE_OF_CHARGE;
 		}
 	} else if (ticketType == 1 && ticketTime == 1 && manAge >= 3 && manAge < 12) {
-		ticketPrice = ALLCHILD1DAY;	
+		price = ALL_CHILD_1DAY;	
 	} else if (ticketType == 1 && ticketTime == 1 && manAge >= 12 && manAge < 18) {
-		ticketPrice = ALLYOUTH1DAY;
+		price = ALL_YOUTH_1DAY;
 	} else if (ticketType == 1 && ticketTime == 1 && manAge >= 18 && manAge < 65) {
-		ticketPrice = ALLADULT1DAY;
+		price = ALL_ADULT_1DAY;
 	} else if (ticketType == 1 && ticketTime == 1 && manAge >= 65) {
-		ticketPrice = ALLCHILD1DAY;
+		price = ALL_CHILD_1DAY;
 	
 	} else if (ticketType == 1 && ticketTime == 2 && manAge >= 3 && manAge < 12) {
-		ticketPrice = ALLCHILDAFTER4;
+		price = ALL_CHILD_AFTER4;
 	} else if (ticketType == 1 && ticketTime == 2 && manAge >= 12 && manAge < 18) {
-		ticketPrice = ALLYOUTHAFTER4;
+		price = ALL_YOUTH_AFTER4;
 	} else if (ticketType == 1 && ticketTime == 2 && manAge >= 18 && manAge < 65) {
-		ticketPrice = ALLADULTAFTER4;
+		price = ALL_ADULT_AFTER4;
 	} else if (ticketType == 1 && ticketTime == 2 && manAge >= 65) {
-		ticketPrice = ALLCHILDAFTER4;
+		price = ALL_CHILD_AFTER4;
 		 
 	} else if (ticketType == 2 && ticketTime == 1 && manAge >= 3 && manAge < 12) {
-		ticketPrice = PARKCHILD1DAY;
+		price = PARK_CHILD_1DAY;
 	} else if (ticketType == 2 && ticketTime == 1 && manAge >= 12 && manAge < 18) {
-		ticketPrice = PARKYOUTH1DAY;
+		price = PARK_YOUTH_1DAY;
 	} else if (ticketType == 2 && ticketTime == 1 && manAge >= 18 && manAge < 65) {
-		ticketPrice = PARKADULT1DAY;
+		price = PARK_ADULT_1DAY;
 	} else if (ticketType == 2 && ticketTime == 1 && manAge >= 65) {
-		ticketPrice = PARKCHILD1DAY;
+		price = PARK_CHILD_1DAY;
 		
 	} else if (ticketType == 2 && ticketTime == 2 && manAge >= 3 && manAge < 12) {
-		ticketPrice = PARKCHILDAFTER4;
+		price = PARK_CHILD_AFTER4;
 	} else if (ticketType == 2 && ticketTime == 2 && manAge >= 12 && manAge < 18) {
-		ticketPrice = PARKYOUTHAFTER4;
+		price = PARK_YOUTH_AFTER4;
 	} else if (ticketType == 2 && ticketTime == 2 && manAge >= 18 && manAge < 65) {
-		ticketPrice = PARKADULTAFTER4;
+		price = PARK_ADULT_AFTER4;
 	} else if (ticketType == 2 && ticketTime == 2 && manAge >= 65) {
-		ticketPrice = PARKCHILDAFTER4;
+		price = PARK_CHILD_AFTER4;
 	}
 	//the number of Tickets(fixed)
 	printf("How many tickets? (max 10)\n");
 	printf("1\n");
 	//Special Offer
-	printf("Special Offer\n1.None\n2.Disabled\n3.Veteran\n4.Military\n5.Pregnant\n6.Multiple\n");
+	printf("Special Offer\n1.None\n2.Disabled\n3.Veteran\n4.Military\n5.Pregnant\n6.MultiChild\n");
 	scanf("%d", &specialOffer);
 	
 	if(manAge >= 65) {
 		specialOffer = 1;
 	}
 	if(specialOffer == 1) {
-		ticketPrice;
+		price;
 	} else if(specialOffer == 2) {
-		ticketPrice*=0.5; 
+		price = (int)(price * DISABLED_DC_RATE);      // if DC_RATE is changed, it might need to round up price 
 	} else if (specialOffer == 3) {
-		ticketPrice*=0.5; 
+		price = (int)(price * VETERAN_DC_RATE);
 	} else if (ticketType == 1 && specialOffer == 4) {
-		ticketPrice*=0.49; 
+		price = (int)(price * MILITARY_DC_RATE); 
 	} else if (ticketType == 1 && specialOffer == 5) {
-		ticketPrice*=0.5; 
+		price = (int)(price * PREGNANT_DC_RATE); 
 	} else if (ticketType == 1 && specialOffer == 6) {
-		ticketPrice*=0.3;
+		price = (int)(price * MULTICHILD_DC_RATE);
 	}
-	printf("The ticket price is : %d\n", ticketPrice);
-	printf("Thank you\n");
+	printf("The ticket price is : %d\n", price);
+	printf("Thank you\n");	
 	//2D array 
-	ticketInfo[repCount][ticketTypeIndex] = ticketType;
-	ticketInfo[repCount][ticketTimeIndex] = ticketTime;
-	ticketInfo[repCount][manAgeIndex] = manAge;
-	ticketInfo[repCount][ticketPriceIndex] = ticketPrice;
-	ticketInfo[repCount][specialOfferIndex] = specialOffer;	
+	ticketInfo[repCount][TICKET_TYPE_INDEX] = ticketType;
+	ticketInfo[repCount][TICKET_TIME_INDEX] = ticketTime;
+	ticketInfo[repCount][MANAGE_INDEX] = manAge;
+	ticketInfo[repCount][PRICE_INDEX] = price;
+	ticketInfo[repCount][SPCIAL_OFFER_INDEX] = specialOffer;	
 		
 	repCount++;
 	
@@ -160,47 +165,47 @@ int main(void) {
 	printf("Stop ticketing. Thank you\n");
 	printf("===================== LOTTE WORLD =====================\n");	
 	for(index = 0; index < repCount; index++) {	
-		if(ticketInfo[index][ticketTypeIndex] == 1) {
+		if(ticketInfo[index][TICKET_TYPE_INDEX] == 1) {
 			printf("All\t");	
-		} else if(ticketInfo[index][ticketTypeIndex] == 2) {
+		} else if(ticketInfo[index][TICKET_TYPE_INDEX] 	== 2) {
 			printf("Park\t");	
 		}
-		if(	ticketInfo[index][ticketTimeIndex] == 1) {
+		if(	ticketInfo[index][TICKET_TIME_INDEX] == 1) {
 			printf("1Day\t");
-		} else if(	ticketInfo[index][ticketTimeIndex] == 2) {
+		} else if(ticketInfo[index][TICKET_TIME_INDEX] == 2) {
 			printf("After4\t");
 		}
 		
-		if(ticketInfo[index][manAgeIndex] < 3) {
+		if(ticketInfo[index][MANAGE_INDEX] < 3) {
 			printf("Baby\t");
-		} else if(ticketInfo[index][manAgeIndex] >= 3 && ticketInfo[index][manAgeIndex] < 12) {
+		} else if(ticketInfo[index][MANAGE_INDEX] >= 3 && ticketInfo[index][MANAGE_INDEX] < 12) {
 			printf("Child\t");
-		} else if(ticketInfo[index][manAgeIndex] >= 12 && ticketInfo[index][manAgeIndex] < 18) {
+		} else if(ticketInfo[index][MANAGE_INDEX] >= 12 && ticketInfo[index][MANAGE_INDEX] < 18) {
 			printf("Youth\t");
-		} else if(ticketInfo[index][manAgeIndex] >= 18 && ticketInfo[index][manAgeIndex] < 65) {
+		} else if(ticketInfo[index][MANAGE_INDEX] >= 18 && ticketInfo[index][MANAGE_INDEX] < 65) {
 			printf("Adult\t");
-		} else if(ticketInfo[index][manAgeIndex] >= 65) {
+		} else if(ticketInfo[index][MANAGE_INDEX] >= 65) {
 			printf("Senior\t");
 		}
 		
-		printf(" X\t%d\t", ticketNum);
-		printf("%d\t",ticketInfo[index][ticketPriceIndex]);
+		printf(" X\t%d\t", qty);
+		printf("%d\t",ticketInfo[index][PRICE_INDEX]);
 		
-		if(ticketInfo[index][specialOfferIndex] == 1) {
+		if(ticketInfo[index][SPCIAL_OFFER_INDEX] == 1) {
 			printf("None\n");
-		} else if(ticketInfo[index][specialOfferIndex] == 2) {
+		} else if(ticketInfo[index][SPCIAL_OFFER_INDEX] == 2) {
 			printf("Disabled\n");
-		} else if(ticketInfo[index][specialOfferIndex] == 3) {
+		} else if(ticketInfo[index][SPCIAL_OFFER_INDEX] == 3) {
 			printf("Veteran\n");
-		} else if(ticketInfo[index][specialOfferIndex] == 4) {
+		} else if(ticketInfo[index][SPCIAL_OFFER_INDEX] == 4) {
 			printf("Military\n");
-		} else if(ticketInfo[index][specialOfferIndex] == 5) {
+		} else if(ticketInfo[index][SPCIAL_OFFER_INDEX] == 5) {
 			printf("Pregnant\n");
-		} else if(ticketInfo[index][specialOfferIndex] == 6) {
-			printf("Multiple\n");
+		} else if(ticketInfo[index][SPCIAL_OFFER_INDEX] == 6) {
+			printf("MultiChild\n");
 		}		
 		
-		totalPrice += ticketInfo[index][ticketPriceIndex];
+		totalPrice += ticketInfo[index][PRICE_INDEX];
 	}
 	printf("The total price is %d\n",totalPrice);
 		totalPrice = 0;
